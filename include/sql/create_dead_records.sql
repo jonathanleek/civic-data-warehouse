7908d1c419b8837667afbd53002f3aeaf765bde2
jonathanleek <jonathanleek@gmail.com>
2023-04-30 13:10:34 -0500

    truncate_staging is working

--- include/sql/create_dead_records.sql
+++
@@ -1,1 +0,0 @@
-create schema if not exists data_prep;
--- include/sql/list_tables_in_schema.sql
+++
@@ -1,5 +0,0 @@
-select t.table_name
-from information_schema.tables t
-where t.table_schema = '{{params.schema}}'  -- put schema name here
-      and t.table_type = 'BASE TABLE'
-order by t.table_name;
