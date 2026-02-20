4b653691b1a73257da65303572c36698ac4bc5f0
KRT <kenneth.r.teague@gmail.com>
2026-02-19 19:17:04 -0600

    Changes for pushing to main proejct - all download and staging fixes to date (02/19/26)

--- include/staging_table_prep.py
+++ include/staging_table_prep.py
@@ -1,26 +1,29 @@
 import os
+import logging
+from logging import Logger
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.utils.log import logging_mixin
 import pandas as pd
 import numpy as np
 from io import StringIO
 import re
 
 
-def download_from_s3(key: str, bucket_name: str, s3_conn_id: str) -> str:
+def download_from_s3(key: str, bucket_name: str, s3_conn_id: str):
+    logger = logging_mixin.LoggingMixin().logger()
     download_dest = "/tmp/"
-    print(download_dest)
     hook = S3Hook(aws_conn_id=s3_conn_id)
     filename = hook.download_file(
         key=key, bucket_name=bucket_name, local_path=download_dest
     )
-    print(filename + " downloaded")
+    logger.info(filename + " downloaded")
     os.rename(src=filename, dst=download_dest + key)
-    print(filename + " renamed to " + download_dest + key)
+    logger.info(filename + " renamed to " + download_dest + key)
 
 
-def execute_query(query, conn_id):
-    hook = PostgresHook(postgres_conn_id=conn_id)
+def execute_query(query, conn_id, logger:Logger):
+    hook = PostgresHook(postgres_conn_id=conn_id, log_sql=(logger.level==logging.DEBUG))
     hook.run(sql=query)
 
 
@@ -138,16 +141,14 @@ def clean_column_name(column_name):
 
 
 def create_table_in_postgres(filename, postgres_conn):
+    logger = logging_mixin.LoggingMixin().logger()
     tablename = filename.replace("/tmp/", "").replace(".csv", "").replace("-", "_")
-    with open(filename, "r") as fileInput:
-        # Extract first line of file
-        firstLine = fileInput.readline().strip()
+    df = pd.read_csv(filename, dtype=str)
 
-    # Split columns into an array
-    columns = firstLine.split(",")
-    print("List of columns:")
-    for column in columns:
-        print(column)
+    columns = [clean_column_name(col) for col in df.columns]
+    logger.info("List of columns:")
+    for col_index, column in enumerate(columns):
+        logger.info(f"column index {col_index} is '{column}'")
 
     # Build SQL code to drop table if exists and create table
     sqlQueryCreate = ""
@@ -160,22 +161,24 @@ def create_table_in_postgres(filename, postgres_conn):
 
     sqlQueryCreate = sqlQueryCreate[:-2]
     sqlQueryCreate += ");"
-    print(sqlQueryCreate)
+    logger.debug(sqlQueryCreate)
 
     # Run sqlQueryCreate in Postgres
-    execute_query(sqlQueryCreate, postgres_conn)
+    execute_query(sqlQueryCreate, postgres_conn, logger)
 
 
 def create_staging_table(bucket, s3_conn_id, postgres_conn_id, key):
+    logger = logging_mixin.LoggingMixin().logger()
     download_from_s3(key=key, bucket_name=bucket, s3_conn_id=s3_conn_id)
-    print("Downloaded " + key)
-    print("Attempting to create table for " + key)
+    logger.info("Downloaded " + key)
+    logger.info("Attempting to create table for " + key)
     create_table_in_postgres(filename="/tmp/" + key, postgres_conn=postgres_conn_id)
 
 
 def SQL_INSERT_STATEMENT_FROM_DATAFRAME(SOURCE, TARGET):
     # Generate the SQL insert statement from dataframe
     sql_texts = []
+    # COPY table (column1, column2, ...) FROM '/path/to/data.csv' WITH (FORMAT CSV)
     for index, row in SOURCE.iterrows():
         cleaned_columns = [clean_column_name(col) for col in SOURCE.columns]
         sql_texts.append(
@@ -190,8 +193,9 @@ def SQL_INSERT_STATEMENT_FROM_DATAFRAME(SOURCE, TARGET):
 
 
 def populate_staging_table(bucket, s3_conn_id, postgres_conn, key):
+    logger = logging_mixin.LoggingMixin().logger()
     # Import table from S3 bucket to a pandas dataframe and convert to an array
-    print("Attempting to populate " + key)
+    logger.info("Attempting to populate " + key)
     hook = S3Hook(aws_conn_id=s3_conn_id)
     obj = hook.read_key(bucket_name=bucket, key=key)
     df = pd.read_csv(StringIO(obj))
@@ -208,6 +212,6 @@ def populate_staging_table(bucket, s3_conn_id, postgres_conn, key):
 
     # Build SQL code to insert data into table
     sqlQueryInsert = SQL_INSERT_STATEMENT_FROM_DATAFRAME(df, tablename)
-    print(sqlQueryInsert)
+    logger.debug(sqlQueryInsert)
     # run sqlQueryCreate in Postgres
-    execute_query(sqlQueryInsert, postgres_conn)
+    execute_query(sqlQueryInsert, postgres_conn, logger)
